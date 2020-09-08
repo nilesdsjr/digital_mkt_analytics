b7aa4158f07b3f70d2d97bd513dcbef0da3ded90
nilesdsjr <niles.deem@gmail.com>
2020-09-08 03:24:34 -0300

    returning response content as json file. it's a dict.

--- extractor.py
+++ extractor.py
@@ -1,14 +1,16 @@
 import requests as r
 
-''' Responsable for the online extraction of data.'''
+''' Responsable for the online extraction of JSON data.'''
 class Extractor:
     
-    def __init__(self)
-        self.url = 'http://dataeng.quero.com:5000/caged-data'
+    def __init__(self):
+        self.url='http://dataeng.quero.com:5000/caged-data'
 
     def url_extract(self):
-        response = r.get(self.url)
-        self.resp_info = response.headers['content-type']
-        if self.resp_info == "application/json"
-            
-        return 
+        response=r.get(self.url)
+        self.resp_info=response.headers['content-type']
+        if self.resp_info=="application/json":
+            return response.json()
+        else:
+            print("Unsupported content-type.")
+        return
