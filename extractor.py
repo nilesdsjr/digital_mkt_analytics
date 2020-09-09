6c8ef925ccc6788870f3ba2398d7965384b15d70
nilesdsjr <niles.deem@gmail.com>
2020-09-09 04:06:02 -0300

    new exception handle for requests

--- extractor.py
+++ extractor.py
@@ -1,53 +1,49 @@
 from connection import Connection
-from settings import LogStream
-import requests as r
+from conf.settings import LogStream
+import time
 
 class Extractor:
-   """
-    Responsable for the online extraction of JSON data. 
+    """
+    Responsable for the web extraction of JSON data.
 
     ...
 
     Attributes
     ----------
-    ROOT_DIR : str
-        a formatted string specifying the path to the root's project. 
-        It's determined by the main class' file location.
-    CONFIG_PATH : str
-        a formatted string specifying the path to the project's conf directory.
-        Used to locate stored files like .yaml, .cfg, .ini, etc.
-    LOG_DIR : str
-        a formatted string specifying the path to the project's log directory.
-        Stores the log file.
+    response : str
+        HTTP response from GET request.
+    url : str
+        a formatted string specifying the API endpoint.
+    t0 : timestamp
+        Marks the begining of the request.
+    t1 : timestamp
+        Marks the ending of the request.
 
     Methods
     -------
-    says(sound=None)
-        Prints the animals name and what sound it makes
+    url_extract(self)
+        Returns the json response as dict. Doesn't return any other type.
     """
-    
     def __init__(self):
-        self.url='http://dataeng.quero.com:5000/caged-data/500'
-        _log_stream = LogStream()
+        self.url='http://dataeng.quero.com:5000/caged-data'
+        _log_stream=LogStream()
         self.log = _log_stream.log_stream(origin=__class__.__name__)
 
     def url_extract(self):
-        
-        t0 = time.time()
+        t0=time.time()
+        conn=Connection()
+        my_session=conn.request_retry_session()
         try:
-            self.log.info('Trying to get API data.')
-            response=r.get(self.url)
-        except requests.exceptions.Timeout:
-            self.log.warning('API timeout. Retrying connection.')
-            response=Connection.requests_retry(self.url)
-        except requests.exceptions.RequestException as e:
-            # catastrophic error. bail.
-            raise SystemExit(e)
-        t1 = time.time()
-        self.log.info('API connection took ', t1 - t0, ' seconds')
-        ct_type=response.headers['content-type']
+            r=my_session.get(self.url)
+        except Exception as e:
+            self.log.error('API not responding right. Check the endpoint:{}'.format(self.url))
+            raise(e)
+        t1=time.time()
+        self.log.info('API connection took {} seconds. STATUS:{}'.format(t1-t0, r.status_code))
+        ct_type=r.headers['content-type']
         if ct_type=="application/json":
-            return response.json()
+            return r.json()
         else:
-            self.log.error('Unsupported content-type {}'.format(ct_type))
-            raise AttributeError('Unsupported content-type {}'.format(ct_type))
+            self.log.info(r.text)
+            self.log.error('Unsupported content-type {} - Only JSON is supported!'.format(ct_type))
+            raise AttributeError('Unsupported content-type {} - Only JSON is supported!'.format(ct_type))
