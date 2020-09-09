3a846553c8a77be529cbb17a158b14a8e687c8a7
nilesdsjr <niles.deem@gmail.com>
2020-09-09 04:05:08 -0300

    calling etl working to show dt

--- etl.py
+++ etl.py
@@ -1,15 +1,23 @@
-from extractor import Extractor
 from transformer import Transformer
 from loader import Loader
+from extractor import Extractor
 
 class Etl:
 
     def run(self):
         _extraction=Extractor()
-        extraction=_extraction.url_extract()
         _tranformation=Transformer()
-        transformation=_tranformation.table_maker(extraction)
+        extraction=_extraction.url_extract()
+        if extraction['success']:
+            transformation=_tranformation.table_maker(extraction['caged'])
+            print(transformation)
+        else:
+            transformation=_tranformation.table_maker(extraction['caged'])
+            print(transformation)
 
 
 
 if __name__ == '__main__':
+
+    etl=Etl()
+    etl.run()
