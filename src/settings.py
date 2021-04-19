788b7d65edf3448b0a0b99a6ad74288d820e2dc6
Niles Deem <niles.deem@gmail.com>
2021-04-19 13:51:34 -0300

    local paths as dict

--- src/settings.py
+++ src/settings.py
@@ -1,19 +1,13 @@
 import os
-import sys
 import logging
 import yaml
-import getpass
-from subprocess import Popen, PIPE
-from datetime import datetime
 
 
 class Settings:
     """
     A general class used to set path options for the runtime.
     It is used to access attributes dinamically through its object.
-
     ...
-
     Attributes
     ----------
     ROOT_DIR : str
@@ -25,26 +19,28 @@ class Settings:
     LOG_DIR : str
         a formatted string specifying the path to the project's log directory.
         Stores the log file.
-
+    CVV_CONFIG_PATH : yaml
+        a config file to load external parameters.
     Methods
     -------
-    says(sound=None)
-        Prints the animals name and what sound it makes
+    
     """
-    def __init__(self, root_file=__file__):
-        self.ROOT_DIR=os.path.dirname(os.path.abspath(root_file))
-        self.CONFIG_PATH=os.path.join(os.path.abspath(os.path.join(self.ROOT_DIR, os.pardir)), 'conf')
-        self.PG_CONFIG_PATH=os.path.join(os.path.abspath(os.path.join(self.ROOT_DIR, os.pardir)), 'conf', 'postgres.yaml')
-        self.LOG_DIR=os.path.join(os.path.abspath(os.path.join(self.ROOT_DIR, os.pardir)), 'logs')
+    def __init__(self):
+
+        self.ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.settings = {
+            'CONFIG_PATH': os.path.join(os.path.abspath(os.path.join(self.ROOT_DIR, os.pardir)), 'resources', 'conf'),
+            'YAML_CONFIG_PATH': os.path.join(os.path.abspath(os.path.join(self.ROOT_DIR, os.pardir)),'resources', 'conf', 'sel.yaml'),
+            'LOG_DIR': os.path.join(os.path.abspath(os.path.join(self.ROOT_DIR, os.pardir)), 'logs'),
+            'DATA_DIR': os.path.join(os.path.abspath(os.path.join(self.ROOT_DIR, os.pardir)), 'data')
+        }
 
 
 class LogStream:
     """
     A intermediate implementation of logging framework used to add logging handlers.
     It helps creating diferent log streams for diferent classes or keeps everything together.
-
     ...
-
     Attributes
     ----------
     settings : Settings object
@@ -52,49 +48,37 @@ class LogStream:
     LOG_DIR : str
         a formatted string specifying the path to the project's log directory.
         Stores the log file.
-
     Methods
     -------
     log_stream(self, origin, _log_dir=settings.LOG_DIR)
         Sets a logger instance with a loaded handler by caller class.
     """
     settings=Settings()
-
-    def __init__(self, settings=settings):
-
-        self.log_dir=settings.LOG_DIR
+    def __init__(self):
+        pass
 
     def log_stream(self, origin, _log_dir=settings.LOG_DIR):
+
         log=logging.getLogger(origin)
         log.setLevel(logging.INFO)
-
         if not os.path.isdir(_log_dir):
-
             os.makedirs(_log_dir)
-
-        handler=logging.FileHandler(os.path.join(_log_dir, 'All_simple_etl_to_local.log'))
+        handler=logging.FileHandler(os.path.join(_log_dir, 'All_base_reclamacao.log'))
         handler.setLevel(logging.INFO)
         formatter=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
-
         if log.handlers:
-
             log.debug('Logging Handler para {} encontrado!'.format(origin))
-
         else:
-
             log.addHandler(handler)
             log.debug('Novo logging Handler para {} adicionado!'.format(origin))
-
         return log
 
 
 class Configuration:
     """
     Specific class do load config files to a python object.
-
     ...
-
     Attributes
     ----------
     config : dict
@@ -103,31 +87,25 @@ class Configuration:
         a cofigurable logger object to be loaded with the caller class parameters.
     log : logger instance
         a formatted string specifying the path to the project's log directory.
-
     Methods
     -------
     load_config(settings=settings)
         loads .yaml file content as dict.
     """
     settings=Settings()
-
-    def load_config(self, settings=settings):
+    def __init__(self):
         _logstream = LogStream()
-        log = _logstream.log_stream(origin=__class__.__name__)
+        self.log = _logstream.log_stream(origin=__class__.__name__)
+
+    def load_config(self, fl_config_path=settings.YAML_CONFIG_PATH):
+
         try:
 
-            with open(settings.PG_CONFIG_PATH, 'rb') as yml:
-
+            with open(fl_config_path, 'rb') as yml:
                 config = yaml.safe_load(yml)
-
         except IOError as e:
-
-            log.error('yaml file loading has faild. Check it at ' +
-                      settings.PG_CONFIG_PATH,
-                      exec_info=True)
-
+            self.log.error('yaml file load has failed. Check it at {}'.format(
+                      fl_config_path), exc_info=True)
             raise(e)
-
-        log.info('yaml file loaded and configurations found.')
-
+        self.log.info('yaml file loaded and configurations found.')
         return config
