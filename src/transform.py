821fe269e8e3e986b8863d1ec0482414ac7967a7
nilesdsjr <niles.deem@gmail.com>
2021-06-15 23:31:12 -0300

    new transform methods, unzip, change types.

--- src/transform.py
+++ src/transform.py
@@ -1,5 +1,10 @@
+import os
+
 import pandas as pd
-from settings import LogStream
+from zipfile import ZipFile
+
+from settings import LogStream, Settings
+
 
 class Transform:
     """
@@ -32,6 +37,8 @@ class Transform:
     def __init__(self):
         _log_stream = LogStream()
         self.log = _log_stream.log_stream(origin=__class__.__name__)
+        settings = Settings()
+        self.local_paths = settings.LOCAL_PATHS
 
 
     def table_maker(self, extract_list):
@@ -68,3 +75,69 @@ class Transform:
         dt=dt.astype(dtype=dtypes)
         self.log.info(dt.dtypes)
         return dt
+
+    def unzip_file(self, zip_path):
+        try:
+            with ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(os.path.join(self.local_paths['DATA_DIR'], 'datasets'))
+        except Exception as e:
+            self.log.error('Something wrong unziping file {}'.format(zip_path), exc_info=True)
+            raise(e)
+    
+    def csv2df(self, csv_path):
+        """Reads a .csv file and loads as pandas DataFrame.
+  
+        Args:
+          csv_path: Absolut path to the .csv file.
+  
+        Returns:
+          A pandas Dataframe.
+  
+        Raises:
+          IOError: If something breaks during loading from local fils system.
+        """
+        try:
+            df = pd.read_csv(csv_path)
+        except IOError as e:
+            self.log.error('Impossible to read file at: {}'.format(csv_path), exc_info=True)
+            raise(e)
+        return df
+
+    def json2df(self, json_path):
+        """Reads a .json file and loads as pandas DataFrame.
+  
+        Args:
+          json_path: Absolut path to the .json file.
+  
+        Returns:
+          A pandas Dataframe.
+  
+        Raises:
+          IOError: If something breaks during loading from local fils system.
+        """
+        try:
+            df = pd.read_json(json_path)
+        except IOError as e:
+            self.log.error('Impossible to read file at: {}'.format(json_path), exc_info=True)
+            raise(e)
+        return df
+
+    def txt2str(self, txt_path):
+        """Reads a .txt file and loads as string.
+  
+        Args:
+          txt_path: Absolut path to the .txt file.
+  
+        Returns:
+          str object.
+  
+        Raises:
+          IOError: If something breaks during loading from local fils system.
+        """
+        try:
+            with open(txt_path, 'r') as f:
+                txt_str = f.read()
+        except IOError as e:
+            self.log.error('Impossible to read file at: {}'.format(txt_path), exc_info=True)
+            raise(e)
+        return txt_str
