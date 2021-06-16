b8f3b363c8cb421b092914011ed9eca63599449c
nilesdsjr <niles.deem@gmail.com>
2021-06-15 23:32:59 -0300

    new docstrings, streaming file download

--- src/extract.py
+++ src/extract.py
@@ -1,42 +1,50 @@
-from connection import Connection
-from settings import LogStream
 import time
+import os
+
+from connection import Connection
+from settings import LogStream, Settings
 
 class Extract:
     """
-    Responsable for the web extraction of JSON data.
+    Responsable for the web extraction of web data.
 
-    ...
-
-    Attributes
-    ----------
-    response : str
-        HTTP response from GET request.
-    url : str
-        a formatted string specifying the API endpoint.
-    t0 : timestamp
-        Marks the begining of the request.
-    t1 : timestamp
-        Marks the ending of the request.
-
-    Methods
-    -------
-    url_extract(self)
-        Returns the json response as dict. Doesn't return any other type.
     """
     def __init__(self):
         _log_stream=LogStream()
         self.log = _log_stream.log_stream(origin=__class__.__name__)
+        settings = Settings()
+        self.local_paths = settings.LOCAL_PATHS
 
     def url_extract(self, url):
+        """ Executes get method from response.Session object.
+        
+        Parameters
+        ----------
+        url : str
+            a formatted string specifying the API endpoint.
+
+        Returns
+        -------
+        requests.Response Object
+            
+        Raises
+        ------
+        Exception
+            Re-raises requests lib error.
+        """
         t0=time.time()
+        local_filename = os.path.join(self.local_paths['DATA_DIR'],'datasets.zip')
         conn=Connection()
         my_session=conn.request_retry_session()
         try:
             r=my_session.get(url, stream=True)
+            r.raise_for_status()
+            with open(local_filename, 'wb') as f:
+               for chunk in r.iter_content(chunk_size=8192): 
+                   f.write(chunk)
         except Exception as e:
-            self.log.error('API not responding right. Check the endpoint:{}'.format(self.url), exc_info=True)
+            self.log.error('API not responding right. Check the endpoint:{}'.format(url), exc_info=True)
             raise(e)
         t1=time.time()
         self.log.info('API connection took {} seconds. STATUS:{}'.format(t1-t0, r.status_code))
-        return r.response
+        return local_filename
